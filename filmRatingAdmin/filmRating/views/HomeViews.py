3b2ba246941ebcd00b2ff2a5c529b052664f3d43
gabriel-liraa <gabriel.l.lira26@gmail.com>
2023-08-14 22:41:16 -0300

    send_request_with_paginator

--- filmRatingAdmin/filmRating/views/HomeViews.py
+++ filmRatingAdmin/filmRating/views/HomeViews.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from filmRating.models.Film import Film
 
 
@@ -9,9 +10,21 @@ def home(request):
     if search is not None and search != "":
         films = Film.objects.filter(name__icontains=search)
 
-    films = films.all()
+    films = films.all().order_by("name")
+
+    get_copy = request.GET.copy()
+    parameters = get_copy.pop("page", True) and get_copy.urlencode()
+
+    if len(films) > 0:
+        paginator = Paginator(films, 3)
+        page_number = request.GET.get("page")
+        films = paginator.get_page(page_number)
 
     context = {
         "films": films,
+        "parameters": parameters,
     }
-    return render(request, "home/home.html", context=context)
+
+    print(parameters)
+
+    return render(request, "home/home.html", context=context, status=200)
